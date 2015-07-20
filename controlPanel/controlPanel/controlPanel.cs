526bb32b846d6b1ab77a012667d417e290394775
chijames <b02902126@ntu.edu.tw>
2015-07-20 15:52:14 +0800

    add control panel for beacons (UI)

--- controlPanel/controlPanel/controlPanel.cs
+++ controlPanel/controlPanel/controlPanel.cs
@@ -15,16 +15,25 @@ namespace controlPanel
     {
         public Result OnStartup(Autodesk.Revit.UI.UIControlledApplication app)
         {
-            RibbonPanel panel = app.CreateRibbonPanel("Beacons");
+            RibbonPanel panel = app.CreateRibbonPanel("Beacon1");
             panel.Enabled = true;
             panel.Visible = true;
-            panel.Name = "first Panel";
-            panel.Title = "addinWizard";
-            PushButtonData pbd = new PushButtonData("cmd1", @"C:\Users\jameschi\Documents\Visual Studio 2013\Projects\pickPoint\pickPoint\bin\Debug\pickPoint.dll", "cmd1", "cmd1");
+            panel.Name = "Beacon Panel1";
+            panel.Title = "Beacons addin Wizard1";
+            PushButtonData pbd = new PushButtonData("Beacon1", "Beacon1", @"C:\Users\jameschi\Documents\Visual Studio 2013\Projects\pickPoint\pickPoint\bin\Debug\pickPoint.dll", "pickPoint");
             PushButton pb = panel.AddItem(pbd) as PushButton;
-            pb.ToolTip = "Ext command1";
+            pb.ToolTip = "Beacon 1";
             panel.AddSeparator();
 
+            RibbonPanel panel2 = app.CreateRibbonPanel("Beacon2");
+            panel2.Enabled = true;
+            panel2.Visible = true;
+            panel2.Name = "Beacon Panel2";
+            panel2.Title = "Beacons addin Wizard2";
+            PushButtonData pbd2 = new PushButtonData("Beacon2", "Beacon2", @"C:\Users\jameschi\Documents\Visual Studio 2013\Projects\pickPoint\pickPoint\bin\Debug\pickPoint.dll", "pickPoint");
+            PushButton pb2 = panel2.AddItem(pbd2) as PushButton;
+            pb2.ToolTip = "Beacon 2";
+            panel2.AddSeparator();
             return Result.Succeeded;
         }
         public Result OnShutdown(UIControlledApplication app)
